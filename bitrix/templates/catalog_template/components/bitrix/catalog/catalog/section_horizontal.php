d3791d8ee3b18388d8ea88f15642a566d1598cd9
Cedigest <Cedigest@yandex.ru>
2015-11-16 13:45:26 +0200

    / section_h / added section el, styles, filter conf
    /sections /
    added section el of all el if no sect
    conf style + bt

--- bitrix/templates/catalog_template/components/bitrix/catalog/catalog/section_horizontal.php
+++ bitrix/templates/catalog_template/components/bitrix/catalog/catalog/section_horizontal.php
@@ -95,7 +95,7 @@ use Bitrix\Main\ModuleManager;
 		?>
 		<?$intSectionID = $APPLICATION->IncludeComponent(
 			"bitrix:catalog.section",
-			"",
+			"catalog_list",
 			array(
 				"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
 				"IBLOCK_ID" => $arParams["IBLOCK_ID"],
@@ -227,63 +227,7 @@ use Bitrix\Main\ModuleManager;
 				if (!isset($arParams['USE_BIG_DATA']) || $arParams['USE_BIG_DATA'] != 'N')
 				{
 					?>
-		<div class="col-xs-12">
-					<?$APPLICATION->IncludeComponent("bitrix:catalog.bigdata.products", "", array(
-						"LINE_ELEMENT_COUNT" => 5,
-						"TEMPLATE_THEME" => (isset($arParams['TEMPLATE_THEME']) ? $arParams['TEMPLATE_THEME'] : ''),
-						"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["element"],
-						"BASKET_URL" => $arParams["BASKET_URL"],
-						"ACTION_VARIABLE" => (!empty($arParams["ACTION_VARIABLE"]) ? $arParams["ACTION_VARIABLE"] : "action")."_cbdp",
-						"PRODUCT_ID_VARIABLE" => $arParams["PRODUCT_ID_VARIABLE"],
-						"PRODUCT_QUANTITY_VARIABLE" => $arParams["PRODUCT_QUANTITY_VARIABLE"],
-						"ADD_PROPERTIES_TO_BASKET" => (isset($arParams["ADD_PROPERTIES_TO_BASKET"]) ? $arParams["ADD_PROPERTIES_TO_BASKET"] : ''),
-						"PRODUCT_PROPS_VARIABLE" => $arParams["PRODUCT_PROPS_VARIABLE"],
-						"PARTIAL_PRODUCT_PROPERTIES" => (isset($arParams["PARTIAL_PRODUCT_PROPERTIES"]) ? $arParams["PARTIAL_PRODUCT_PROPERTIES"] : ''),
-						"SHOW_OLD_PRICE" => $arParams['SHOW_OLD_PRICE'],
-						"SHOW_DISCOUNT_PERCENT" => $arParams['SHOW_DISCOUNT_PERCENT'],
-						"PRICE_CODE" => $arParams["PRICE_CODE"],
-						"SHOW_PRICE_COUNT" => $arParams["SHOW_PRICE_COUNT"],
-						"PRODUCT_SUBSCRIPTION" => $arParams['PRODUCT_SUBSCRIPTION'],
-						"PRICE_VAT_INCLUDE" => $arParams["PRICE_VAT_INCLUDE"],
-						"USE_PRODUCT_QUANTITY" => $arParams['USE_PRODUCT_QUANTITY'],
-						"SHOW_NAME" => "Y",
-						"SHOW_IMAGE" => "Y",
-						"MESS_BTN_BUY" => $arParams['MESS_BTN_BUY'],
-						"MESS_BTN_DETAIL" => $arParams['MESS_BTN_DETAIL'],
-						"MESS_BTN_SUBSCRIBE" => $arParams['MESS_BTN_SUBSCRIBE'],
-						"MESS_NOT_AVAILABLE" => $arParams['MESS_NOT_AVAILABLE'],
-						"PAGE_ELEMENT_COUNT" => 5,
-						"SHOW_FROM_SECTION" => "Y",
-						"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-						"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-						"DEPTH" => "2",
-						"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-						"CACHE_TIME" => $arParams["CACHE_TIME"],
-						"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-						"SHOW_PRODUCTS_".$arParams["IBLOCK_ID"] => "Y",
-						"HIDE_NOT_AVAILABLE" => $arParams["HIDE_NOT_AVAILABLE"],
-						"CONVERT_CURRENCY" => $arParams["CONVERT_CURRENCY"],
-						"CURRENCY_ID" => $arParams["CURRENCY_ID"],
-						"SECTION_ID" => $intSectionID,
-						"SECTION_CODE" => "",
-						"SECTION_ELEMENT_ID" => "",
-						"SECTION_ELEMENT_CODE" => "",
-						"LABEL_PROP_".$arParams["IBLOCK_ID"] => $arParams['LABEL_PROP'],
-						"PROPERTY_CODE_".$arParams["IBLOCK_ID"] => $arParams["LIST_PROPERTY_CODE"],
-						"PROPERTY_CODE_".$arRecomData['OFFER_IBLOCK_ID'] => $arParams["LIST_OFFERS_PROPERTY_CODE"],
-						"CART_PROPERTIES_".$arParams["IBLOCK_ID"] => $arParams["PRODUCT_PROPERTIES"],
-						"CART_PROPERTIES_".$arRecomData['OFFER_IBLOCK_ID'] => $arParams["OFFERS_CART_PROPERTIES"],
-						"ADDITIONAL_PICT_PROP_".$arParams["IBLOCK_ID"] => $arParams['ADD_PICT_PROP'],
-						"ADDITIONAL_PICT_PROP_".$arRecomData['OFFER_IBLOCK_ID'] => $arParams['OFFER_ADD_PICT_PROP'],
-						"OFFER_TREE_PROPS_".$arRecomData['OFFER_IBLOCK_ID'] => $arParams["OFFER_TREE_PROPS"],
-						"RCM_TYPE" => (isset($arParams['BIG_DATA_RCM_TYPE']) ? $arParams['BIG_DATA_RCM_TYPE'] : '')
-					),
-					$component,
-					array("HIDE_ICONS" => "Y")
-				);
-				?>
-		</div>
-				<?
+						<?
 				}
 			}
 		}
---
+++ bitrix/templates/catalog_template/components/bitrix/catalog/catalog/sections.php
@@ -0,0 +1,240 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
+$this->setFrameMode(true);
+$this->addExternalCss("/bitrix/css/main/bootstrap.css");
+?>
+	<span class="cat_title">Новые поступления</span>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.section", 
+	"catalog_list", 
+	array(
+		"ACTION_VARIABLE" => "action",
+		"ADD_PICT_PROP" => "MORE_PHOTO",
+		"ADD_PROPERTIES_TO_BASKET" => "Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"ADD_TO_BASKET_ACTION" => "ADD",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BACKGROUND_IMAGE" => "-",
+		"BASKET_URL" => "/personal/basket.php",
+		"BROWSER_TITLE" => "-",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"COMPONENT_TEMPLATE" => "catalog_list",
+		"CONVERT_CURRENCY" => "N",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_FIELD2" => "id",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"ELEMENT_SORT_ORDER2" => "desc",
+		"FILTER_NAME" => "chosenItems",
+		"HIDE_NOT_AVAILABLE" => "N",
+		"IBLOCK_ID" => "2",
+		"IBLOCK_TYPE" => "catalog",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"LABEL_PROP" => "NEWPRODUCT",
+		"LINE_ELEMENT_COUNT" => "3",
+		"MESSAGE_404" => "",
+		"MESS_BTN_ADD_TO_BASKET" => "В корзину",
+		"MESS_BTN_BUY" => "Купить",
+		"MESS_BTN_DETAIL" => "Подробнее",
+		"MESS_BTN_SUBSCRIBE" => "Подписаться",
+		"MESS_NOT_AVAILABLE" => "Нет в наличии",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"OFFERS_CART_PROPERTIES" => array(
+			0 => "ARTNUMBER",
+		),
+		"OFFERS_FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"OFFERS_LIMIT" => "5",
+		"OFFERS_PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"OFFERS_SORT_FIELD" => "sort",
+		"OFFERS_SORT_FIELD2" => "id",
+		"OFFERS_SORT_ORDER" => "asc",
+		"OFFERS_SORT_ORDER2" => "desc",
+		"OFFER_ADD_PICT_PROP" => "MORE_PHOTO",
+		"OFFER_TREE_PROPS" => array(
+			0 => "COLOR_REF",
+			1 => "SIZES_SHOES",
+			2 => "SIZES_CLOTHES",
+		),
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Товары",
+		"PAGE_ELEMENT_COUNT" => "30",
+		"PARTIAL_PRODUCT_PROPERTIES" => "N",
+		"PRICE_CODE" => array(
+			0 => "BASE",
+		),
+		"PRICE_VAT_INCLUDE" => "Y",
+		"PRODUCT_DISPLAY_MODE" => "Y",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"PRODUCT_PROPERTIES" => array(
+		),
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"PRODUCT_QUANTITY_VARIABLE" => "",
+		"PRODUCT_SUBSCRIPTION" => "N",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"SECTION_CODE" => "",
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],
+		"SECTION_ID_VARIABLE" => "SECTION_ID",
+		"SECTION_URL" => "",
+		"SECTION_USER_FIELDS" => array(
+			0 => "",
+			1 => "",
+		),
+		"SEF_MODE" => "N",
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SHOW_ALL_WO_SECTION" => "Y",
+		"SHOW_CLOSE_POPUP" => "N",
+		"SHOW_DISCOUNT_PERCENT" => "N",
+		"SHOW_OLD_PRICE" => "N",
+		"SHOW_PRICE_COUNT" => "1",
+		"TEMPLATE_THEME" => "blue",
+		"USE_MAIN_ELEMENT_SECTION" => "N",
+		"USE_PRICE_COUNT" => "N",
+		"USE_PRODUCT_QUANTITY" => "N"
+	),
+	false
+);?>
+
+<?
+if($arParams["USE_COMPARE"]=="Y")
+{
+	?><?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.compare.list",
+	"",
+	array(
+		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+		"NAME" => $arParams["COMPARE_NAME"],
+		"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["element"],
+		"COMPARE_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["compare"],
+		"ACTION_VARIABLE" => $arParams["ACTION_VARIABLE"],
+		"PRODUCT_ID_VARIABLE" => $arParams["PRODUCT_ID_VARIABLE"],
+		'POSITION_FIXED' => isset($arParams['COMPARE_POSITION_FIXED']) ? $arParams['COMPARE_POSITION_FIXED'] : '',
+		'POSITION' => isset($arParams['COMPARE_POSITION']) ? $arParams['COMPARE_POSITION'] : ''
+	),
+	$component,
+	array("HIDE_ICONS" => "Y")
+);?><?
+}
+if($arParams["SHOW_TOP_ELEMENTS"]!="N")
+{
+	if (isset($arParams['USE_COMMON_SETTINGS_BASKET_POPUP']) && $arParams['USE_COMMON_SETTINGS_BASKET_POPUP'] == 'Y')
+	{
+		$basketAction = (isset($arParams['COMMON_ADD_TO_BASKET_ACTION']) ? $arParams['COMMON_ADD_TO_BASKET_ACTION'] : '');
+	}
+	else
+	{
+		$basketAction = (isset($arParams['TOP_ADD_TO_BASKET_ACTION']) ? $arParams['TOP_ADD_TO_BASKET_ACTION'] : '');
+	}
+
+	?><?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.top",
+	"",
+	array(
+		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+		"ELEMENT_SORT_FIELD" => $arParams["TOP_ELEMENT_SORT_FIELD"],
+		"ELEMENT_SORT_ORDER" => $arParams["TOP_ELEMENT_SORT_ORDER"],
+		"ELEMENT_SORT_FIELD2" => $arParams["TOP_ELEMENT_SORT_FIELD2"],
+		"ELEMENT_SORT_ORDER2" => $arParams["TOP_ELEMENT_SORT_ORDER2"],
+		"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+		"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["element"],
+		"BASKET_URL" => $arParams["BASKET_URL"],
+		"ACTION_VARIABLE" => $arParams["ACTION_VARIABLE"],
+		"PRODUCT_ID_VARIABLE" => $arParams["PRODUCT_ID_VARIABLE"],
+		"SECTION_ID_VARIABLE" => $arParams["SECTION_ID_VARIABLE"],
+		"PRODUCT_QUANTITY_VARIABLE" => $arParams["PRODUCT_QUANTITY_VARIABLE"],
+		"PRODUCT_PROPS_VARIABLE" => $arParams["PRODUCT_PROPS_VARIABLE"],
+		"DISPLAY_COMPARE" => $arParams["USE_COMPARE"],
+		"ELEMENT_COUNT" => $arParams["TOP_ELEMENT_COUNT"],
+		"LINE_ELEMENT_COUNT" => $arParams["TOP_LINE_ELEMENT_COUNT"],
+		"PROPERTY_CODE" => $arParams["TOP_PROPERTY_CODE"],
+		"PRICE_CODE" => $arParams["PRICE_CODE"],
+		"USE_PRICE_COUNT" => $arParams["USE_PRICE_COUNT"],
+		"SHOW_PRICE_COUNT" => $arParams["SHOW_PRICE_COUNT"],
+		"PRICE_VAT_INCLUDE" => $arParams["PRICE_VAT_INCLUDE"],
+		"PRICE_VAT_SHOW_VALUE" => $arParams["PRICE_VAT_SHOW_VALUE"],
+		"USE_PRODUCT_QUANTITY" => $arParams['USE_PRODUCT_QUANTITY'],
+		"ADD_PROPERTIES_TO_BASKET" => (isset($arParams["ADD_PROPERTIES_TO_BASKET"]) ? $arParams["ADD_PROPERTIES_TO_BASKET"] : ''),
+		"PARTIAL_PRODUCT_PROPERTIES" => (isset($arParams["PARTIAL_PRODUCT_PROPERTIES"]) ? $arParams["PARTIAL_PRODUCT_PROPERTIES"] : ''),
+		"PRODUCT_PROPERTIES" => $arParams["PRODUCT_PROPERTIES"],
+		"CACHE_TYPE" => $arParams["CACHE_TYPE"],
+		"CACHE_TIME" => $arParams["CACHE_TIME"],
+		"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+		"OFFERS_CART_PROPERTIES" => $arParams["OFFERS_CART_PROPERTIES"],
+		"OFFERS_FIELD_CODE" => $arParams["TOP_OFFERS_FIELD_CODE"],
+		"OFFERS_PROPERTY_CODE" => $arParams["TOP_OFFERS_PROPERTY_CODE"],
+		"OFFERS_SORT_FIELD" => $arParams["OFFERS_SORT_FIELD"],
+		"OFFERS_SORT_ORDER" => $arParams["OFFERS_SORT_ORDER"],
+		"OFFERS_SORT_FIELD2" => $arParams["OFFERS_SORT_FIELD2"],
+		"OFFERS_SORT_ORDER2" => $arParams["OFFERS_SORT_ORDER2"],
+		"OFFERS_LIMIT" => $arParams["TOP_OFFERS_LIMIT"],
+		'CONVERT_CURRENCY' => $arParams['CONVERT_CURRENCY'],
+		'CURRENCY_ID' => $arParams['CURRENCY_ID'],
+		'HIDE_NOT_AVAILABLE' => $arParams['HIDE_NOT_AVAILABLE'],
+		'VIEW_MODE' => (isset($arParams['TOP_VIEW_MODE']) ? $arParams['TOP_VIEW_MODE'] : ''),
+		'ROTATE_TIMER' => (isset($arParams['TOP_ROTATE_TIMER']) ? $arParams['TOP_ROTATE_TIMER'] : ''),
+		'TEMPLATE_THEME' => (isset($arParams['TEMPLATE_THEME']) ? $arParams['TEMPLATE_THEME'] : ''),
+		'LABEL_PROP' => $arParams['LABEL_PROP'],
+		'ADD_PICT_PROP' => $arParams['ADD_PICT_PROP'],
+		'PRODUCT_DISPLAY_MODE' => $arParams['PRODUCT_DISPLAY_MODE'],
+
+		'OFFER_ADD_PICT_PROP' => $arParams['OFFER_ADD_PICT_PROP'],
+		'OFFER_TREE_PROPS' => $arParams['OFFER_TREE_PROPS'],
+		'PRODUCT_SUBSCRIPTION' => $arParams['PRODUCT_SUBSCRIPTION'],
+		'SHOW_DISCOUNT_PERCENT' => $arParams['SHOW_DISCOUNT_PERCENT'],
+		'SHOW_OLD_PRICE' => $arParams['SHOW_OLD_PRICE'],
+		'MESS_BTN_BUY' => $arParams['MESS_BTN_BUY'],
+		'MESS_BTN_ADD_TO_BASKET' => $arParams['MESS_BTN_ADD_TO_BASKET'],
+		'MESS_BTN_SUBSCRIBE' => $arParams['MESS_BTN_SUBSCRIBE'],
+		'MESS_BTN_DETAIL' => $arParams['MESS_BTN_DETAIL'],
+		'MESS_NOT_AVAILABLE' => $arParams['MESS_NOT_AVAILABLE'],
+		'ADD_TO_BASKET_ACTION' => $basketAction,
+		'SHOW_CLOSE_POPUP' => isset($arParams['COMMON_SHOW_CLOSE_POPUP']) ? $arParams['COMMON_SHOW_CLOSE_POPUP'] : '',
+		'COMPARE_PATH' => $arResult['FOLDER'].$arResult['URL_TEMPLATES']['compare']
+	),
+	$component
+);?><?
+	unset($basketAction);
+}
+?>
